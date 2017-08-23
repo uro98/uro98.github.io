bf57f9e2396b60a5754e2f2c578fa47821cb73a2
Yu Jo <zoey5538@gmail.com>
2017-08-23 22:00:09 +0200

    Change nav bar alpha channel on scroll

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
 var showing = false;
+var transparent = true; //nav bar transparency
+
+$(document).ready(function() {
+  if(transparent == false) {
+    $('.main-header').css('background', 'rgba(23, 22, 24, 0.92)');
+  }
+
+  $(window).scroll(function() {
+    //if scrolled window at least 10px, make nav bar bg not transparent
+    if($(window).scrollTop() < 10) {
+      $('.main-header').css('background', 'rgba(23, 22, 24, 0)');
+      $('.main-nav-links').removeClass('hover-bg');
+      transparent = true;
+    } else {
+      $('.main-header').css('background', 'rgba(23, 22, 24, 0.92)');
+      //make nav bar links bg on hover not transparent
+      $('.main-nav-links').addClass('hover-bg');
+      transparent = false;
+    }
+  })
+})
 
 function scroll(id) {
   //scroll to element with id
