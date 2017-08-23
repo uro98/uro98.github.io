4308c1b571a34de7c98a86379336007748f444bc
Yu Jo <zoey5538@gmail.com>
2017-08-23 18:47:17 +0200

    Add games list, change project page and footer layout

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
+var showing = false;
+
 function scroll(id) {
   //scroll to element with id
   $(window).scrollTo(document.getElementById(id), 500);
+}
+
+function hideSection(section) {
+  $("#".concat(section)).slideUp(200).css('opacity', 0);
+}
+
+function showSection(section) {
+  $("#".concat(section)).slideDown(200).css('opacity', 1);
+}
+
+//toggle show/hide section
+function toggleSection(section) {
+  if (showing == true) {
+    hideSection(section);
+    showing = false;
+  } else {
+    showSection(section);
+    showing = true;
+  }
 }
